62f33c5498a613e71ec9413a5506276413515b35
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-31 14:03:25 -0700

    MinoMode.py
    The script file for Mino mode, the singular cube placing mode.

---
+++ Engine/MinoMode/MinoMode.py
@@ -0,0 +1,39 @@
+# Start of script
+import math
+import random
+import pygui # I don't know any graphics libraries, so this is filler
+# Mino
+def minoClassic():
+  # A classic mino piece. Not yet available
+# Score
+curScoreMinoClassicMode = int(0)
+highScoreMinoClassicMode = int(0)
+# Pieces
+curPiece = str("Mino")
+curPieceID = float(1.0)
+print("Next:\n" + str(curPiece))
+'''
+Piece ID list or Mino mode:
+mino(white) - 1.0
+mino(black) - 1.1
+mino(grey) - 1.2
+mino(ruby red) - 1.3
+mino(olive green) - 1.4
+mino(silver) - 1.5
+mino(orange) - 1.6
+mino(yellow) - 1.7
+mino(salmon pink) - 1.8
+mino(magenta) - 1.9
+mino(cyan) - 1.10
+mino(turquiose) - 1.11
+mino(brown) - 1.12
+mino(bronze) - 1.13
+mino(gold) - 1.14
+mino(purple) - 1.15
+16 IDs for colored Mino pieces
+'''
+# Run
+print("This script is highly incomplete. Please close the program")
+noMore = input("Press [ENTER] key to quit")
+print("Goodbye!")
+# End of script
