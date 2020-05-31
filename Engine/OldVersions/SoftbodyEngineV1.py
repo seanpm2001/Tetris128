9f87a80414f4ac75811b34a714313a356475cd8b
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-31 13:56:10 -0700

    SoftbodyEngineV1.py
    An archive of version 1 of the softbody graphics engine.

---
+++ Engine/OldVersions/SoftbodyEngineV1.py
@@ -0,0 +1,55 @@
+# Start of script
+import math
+import random
+import pygui # This likely doesn't work, I don't know any GUI libraries
+# Mino (1)
+def softbodyMino():
+  # This script is for the mino piece, a custom piece that is only 1x1
+# Domino (2)
+def softbodyDomino():
+  # This script is for the domino piece
+# Trominoes (3)
+def softbodyTrominoAngle3piece():
+  # This script is for the tromino angle 3 piece |_
+def softbodyTromino3LinePiece():
+  # This script is for the Tromino 3 line piece |
+# Tetrominoes (4)
+def softbodyLine4piece():
+  # This script is for the Tetromino line piece |
+def softbodySquare4Piece():
+  # This script is for the Tetromino square piece ::
+def softbodyL4piece():
+  # This script is for the Tetromino square piece L
+def softbodyInverseSkew4piece():
+  # This script is for the Tetromino Inverse skew piece []
+def softbodyTSpin4piece():
+  # This script is for the Tetromino T-SPin piece
+# Pentominoes (5)
+def pentomino5LinePiece():
+  # This script is for the Pentomino 5 line piece |
+# More Coming soon (I don't know how to name or place this many pieces yet)
+# Hexominoes (6)
+def hexomino6LinePiece():
+  # This script is for the Hexomino 6 line piece |
+# More Coming soon (I don't know how to name or place this many pieces yet)
+# Septominoes (7)
+def septomino7LinePiece():
+  # This script is for the septomino 7 line piece |
+# More Coming soon (I don't know how to name or place this many pieces yet)
+# Octominoes (8)
+def octomino8LinePiece():
+  # This script is for the octomino 8 line piece |
+# More Coming soon (I don't know how to name or place this many pieces yet)
+# Nonominoes (9)
+def nonomino9LinePiece():
+  # This script is for the nonomino 9 line piece |
+# More Coming soon (I don't know how to name or place this many pieces yet)
+# Decominoes (10)
+def decomino10LinePiece():
+  # This script is for the decomino 10 line piece |
+# More Coming soon (I don't know how to name or place this many pieces yet)
+# Run
+print("This program is highly incomplete. Please exit")
+noMore = input("Press [ENTER] key to exit")
+print("Goodbye")
+# End of script
