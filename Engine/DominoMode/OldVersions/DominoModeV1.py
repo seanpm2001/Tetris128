fa8ba6ffc089bf458a35f2e9d1e480a89719da79
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-31 14:07:45 -0700

    DominoModeV1.py
    An archive of version 1 of the Domino mode script.

---
+++ Engine/DominoMode/OldVersions/DominoModeV1.py
@@ -0,0 +1,39 @@
+# Start of script
+import math
+import random
+import pygui # I don't know any graphics libraries, so this is filler
+# Domino
+def DominoClassic():
+  # A classic Domino piece. Not yet available
+# Score
+curScoreDominoClassicMode = int(0)
+highScoreDominoClassicMode = int(0)
+# Pieces
+curPiece = str("Domino")
+curPieceID = float(2.0)
+print("Next:\n" + str(curPiece))
+'''
+Piece ID list or Domino mode:
+Domino(white) - 2.0
+Domino(black) - 2.1
+Domino(grey) - 2.2
+Domino(ruby red) - 2.3
+Domino(olive green) - 2.4
+Domino(silver) - 2.5
+Domino(orange) - 2.6
+Domino(yellow) - 2.7
+Domino(salmon pink) - 2.8
+Domino(magenta) - 2.9
+Domino(cyan) - 2.10
+Domino(turquiose) - 2.11
+Domino(brown) - 2.12
+Domino(bronze) - 2.13
+Domino(gold) - 2.14
+Domino(purple) - 2.15
+16 IDs for colored Domino pieces
+'''
+# Run
+print("This script is highly incomplete. Please close the program")
+noMore = input("Press [ENTER] key to quit")
+print("Goodbye!")
+# End of script
