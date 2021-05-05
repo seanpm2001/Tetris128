8d276bb5c1cb0be7d3c09b6b70674dc4fc591d38
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-04 22:45:14 -0700

    PIECE_PLACEMENT_V1.py
    An archive of the first version of the piece placement script for this directory.

---
+++ Engine/LIBraries/PiecePlacement/Piece_Placement.py/OldVersions/1/1-100/PIECE_PLACEMENT_V1.py
@@ -0,0 +1,57 @@
+# Start of script
+# Taken from my overnight notes from May 2021 (May 1st-May 3rd 2021)
+# This file is highly incomplete. I was in a rush and put concepts in other languages in places where I didn't know how to do so in Python.
+# A lot needs to be fixed. This is non-functional bad code.
+class piecePlacement
+# class softbodyTetris
+import random
+# import tetris128.physics()
+def currentPieceParty():
+  currentPieceTypeGetter = int(random.randint(0,9))
+  currentPieceGet = ["Mino", "Domino", "Tromino", "Tetromino", "Pentomino", "Hexomino", "Septomino", "Heptomino", "Octomino", "Nonomino", "Decomino"]
+  # if (currentPieceTypeGetter == 0):
+  currentPiece = array.currentPieceGet(currentPieceTypeGetter) # Not valid syntax
+  # elif (currentPieceTypeGetter == 1):
+  # Incomplete, I don't have time tonight
+def nextPiece():
+  nextPieceTypeGetter = int(random.randint(0,9))
+  nextPieceGet = ["Mino", "Domino", "Tromino", "Tetromino", "Pentomino", "Hexomino", "Septomino", "Heptomino", "Octomino", "Nonomino", "Decomino"]
+  # if (nextPieceTypeGetter == 0):
+  nextPiece = array.nextPieceGet(nextPieceTypeGetter) # Not valid syntax
+  # elif (nextPieceTypeGetter == 1):
+  # Incomplete, I don't have time tonight
+def placement():
+  # Incomplete, I don't have time tonight
+def rotate():
+  rotate1 = bool("True")
+  rotate2 = bool("False")
+  rotate3 = bool("False")
+  rotate4 = bool("False")
+  # Incomplete, I don't have time tonight
+def place():
+  place()
+  # Incomplete, I don't have time tonight
+def flip():
+  rotate()
+  # Incomplete, I don't have time tonight
+def gameOver():
+  if (str(capsule == "full")):
+      print("Game over")
+  # Incomplete, I don't have time tonight
+def tetris():
+  if (hex(capsule == "0x10", "0x10", "0x10", "0x10")):
+      console.log("Tetris!");
+  # Incomplete, I don't have time tonight
+def music():
+  play ogg.tetris(select.userInput);
+  # Incomplete, I don't have time tonight
+def nextPiece():
+
+# def Etc.
+
+""" File info
+File type: Python source file (*.py)
+File version: 1 (Tuesday, May 4th 2021 at 10:44 pm)
+Line count (including blank lines and compiler line): 58
+"""
+# End of script
