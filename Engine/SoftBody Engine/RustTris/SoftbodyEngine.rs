9250e890d60cad6d54f83f78ac60a23fe5c8125a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-05-05 17:19:01 -0700

    SoftbodyEngine.rs
    The softbody engine so far rewritten in Rust.

---
+++ Engine/SoftBody Engine/RustTris/SoftbodyEngine.rs
@@ -0,0 +1,101 @@
+// Start of script
+// import math
+// import random
+// import pygui # This likely doesn't work, I don't know any GUI libraries
+// Mino (1)
+fn softbodyMino() {
+  // This script is for the mino piece, a custom piece that is only 1x1
+  println!("Mino mode is currently unavailable for the softbody engine.");
+}
+// Domino (2)
+fn softbodyDomino() {
+  // This script is for the domino piece
+  println!("Domino mode is currently unavailable for the softbody engine.");
+}
+// Trominoes (3)
+fn softbodyTromino() {
+  println!("Tromino mode is currently unavailable for the softbody engine.");
+  fn softbodyTrominoAngle3piece() {
+    // This script is for the tromino angle 3 piece |_
+  }
+  fn softbodyTromino3LinePiece() {
+    // This script is for the Tromino 3 line piece |
+  }
+}  	
+// Tetrominoes (4)
+fn softbodyTetromino() {
+  println!("Tetromino mode is currently unavailable for the softbody engine");
+  fn softbodyLine4piece() {
+    // This script is for the Tetromino line piece |
+  }
+  fn softbodySquare4Piece() {
+    // This script is for the Tetromino square piece ::
+  }
+  fn softbodyL4piece() {
+    // This script is for the Tetromino square piece L
+  }
+  fn softbodyInverseSkew4piece() {
+    // This script is for the Tetromino Inverse skew piece []
+  }
+  fn softbodyTSpin4piece() {
+    // This script is for the Tetromino T-Spin piece
+  }
+}
+// Pentominoes (5)
+fn softbodyPentomino() {
+  println!("Pentomino mode is currently unavailable for the softbody engine");
+  fn pentomino5LinePiece() {
+    // This script is for the Pentomino 5 line piece |
+  }
+  // More Coming soon (I don't know how to name or place this many pieces yet)
+}
+// Hexominoes (6)
+fn softbodyHexomino() {
+  println!("Hexomino mode is currently unavailable for the softbody engine");
+  fn hexomino6LinePiece() {
+    // This script is for the Hexomino 6 line piece |
+  }
+// More Coming soon (I don't know how to name or place this many pieces yet)
+}
+// Septominoes (7)
+fn softbodyHeptomino() {
+  println!("Heptomino mode is currently unavailable for the softbody engine");
+  fn septomino7LinePiece() {
+    // This script is for the septomino 7 line piece |
+    }
+// More Coming soon (I don't know how to name or place this many pieces yet)
+}
+// Octominoes (8)
+fn softbodyOctomino() {
+  println!("Octomino mode is currently unavailable for the softbody engine");
+  fn octomino8LinePiece() {
+    // This script is for the octomino 8 line piece |
+  }
+// More Coming soon (I don't know how to name or place this many pieces yet)
+}
+// Nonominoes (9)
+fn softbodyNonomino() {
+  println!("Nonomino mode is currently unavailable for the softbody engine");
+  fn nonomino9LinePiece() {
+    // This script is for the nonomino 9 line piece |
+  }
+// More Coming soon (I don't know how to name or place this many pieces yet)
+}
+// Decominoes (10)
+fn softbodyDecomino() {
+  println!("Decomino mode is currently unavailable for the softbody engine");
+  fn decomino10LinePiece() {
+    // This script is for the decomino 10 line piece |
+  }
+// More Coming soon (I don't know how to name or place this many pieces yet)
+}
+// Run
+println!("This program is highly incomplete. Please exit");
+// noMore = input("Press [ENTER] key to exit")
+// print("Goodbye")
+/* File info
+* File type: Rust source file (*.rs)
+* File version: 1 (Wednesday, May 5th 2021 at 5:19 pm)
+* Line count (including blank lines and compiler line): 102
+*/
+// End of script
